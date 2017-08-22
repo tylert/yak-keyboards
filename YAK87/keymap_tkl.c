a83a3b18998090e5b2709cba4641b77f6e06a579
Tyler Tidman <tyler.tidman@draak.ca>
2017-08-22 15:17:22 -0400

    Shuffle things around and add Workman layer

--- YAK87/keymap_tkl.c
+++ YAK87/keymap_tkl.c
@@ -16,8 +16,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
-static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0:  Qwerty Keys
+#include "keymap_tkl.h"
+
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+/* Keymap 0:  Qwerty and common Keys
  * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
  * |Esc|   |F1 |F2 |F3 |F4 | |F5 |F6 |F7 |F8 | |F9 |F10|F11|F12| |PrS|ScL|Pau|
  * `---'   `---------------' `---------------' `---------------' `-----------'
@@ -30,7 +33,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------|     ,---.
  * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift     |     |Up |
  * |-----------------------------------------------------------| ,-----------.
- * |Ctl|Gui|Alt|          Space                |Alt|Gui|Fn3|Ctl| |Lft|Dwn|Rgt|
+ * |Ctl|Gui|Alt|          Space                |Alt|Gui|Fn4|Ctl| |Lft|Dwn|Rgt|
  * `-----------------------------------------------------------' `-----------'
  */
     KEYMAP( \
@@ -39,7 +42,7 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,DEL, END, PGDN,  \
         CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                  \
         LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,     UP,         \
-        LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN3, RCTL,LEFT,DOWN,RGHT), \
+        LCTL,LGUI,LALT,               SPC,                RALT,RGUI,FN4, RCTL,LEFT,DOWN,RGHT), \
 /* Keymap 1:  Colemak Keys
  * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
  * |   |   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |
@@ -58,12 +61,35 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     KEYMAP( \
         TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-        TRNS,TRNS,TRNS,F,   P,   G,   J,   L,   U,   Y,   SCLN,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
-        TRNS,TRNS,R,   S,   T,   D,   TRNS,N,   E,   I,   O,   TRNS,     TRNS,                 \
-        TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,K,   TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,       \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MINS,EQL, TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,Q,   W,   F,   P,   G,   J,   L,   U,   Y,   SCLN,LBRC,RBRC,TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,A,   R,   S,   T,   D,   H,   N,   E,   I,   O,   QUOT,     TRNS,                 \
+        TRNS,     Z,   X,   C,   V,   B,   K,   M,   COMM,DOT, SLSH,     TRNS,     TRNS,       \
         TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS), \
-/* Keymap 2:  Dvorak Keys
+/* Keymap 2:  Workman Keys
+ * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
+ * |   |   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |
+ * `---'   `---------------' `---------------' `---------------' `-----------'
+ * ,-----------------------------------------------------------. ,-----------.
+ * |   |   |   |   |   |   |   |   |   |   |   |  -|  =|       | |   |   |   |
+ * |-----------------------------------------------------------| |-----------|
+ * |     |  Q|  D|  R|  W|  B|  J|  F|  U|  P|  ;|  [|  ]|     | |   |   |   |
+ * |-----------------------------------------------------------| `-----------'
+ * |      |  A|  S|  H|  T|  G|  Y|  N|  E|  O|  I|  '|        |
+ * |-----------------------------------------------------------|     ,---.
+ * |        |  Z|  X|  M|  C|  V|  K|  L|  ,|  .|  /|          |     |   |
+ * |-----------------------------------------------------------| ,-----------.
+ * |   |   |   |                               |   |   |   |   | |   |   |   |
+ * `-----------------------------------------------------------' `-----------'
+ */
+    KEYMAP( \
+        TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MINS,EQL, TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,Q,   D,   R,   W,   B,   J,   F,   U,   P,   SCLN,LBRC,RBRC,TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,A,   S,   H,   T,   G,   Y,   N,   E,   O,   I,   QUOT,     TRNS,                 \
+        TRNS,     Z,   X,   M,   C,   V,   K,   L,   COMM,DOT, SLSH,     TRNS,     TRNS,       \
+        TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS), \
+/* Keymap 3:  Dvorak Keys
  * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
  * |   |   |   |   |   |   | |   |   |   |   | |   |   |   |   | |   |   |   |
  * `---'   `---------------' `---------------' `---------------' `-----------'
@@ -82,40 +108,41 @@ static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP( \
         TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,LBRC,RBRC,TRNS,TRNS,TRNS,TRNS,  \
-        TRNS,QUOT,COMM,DOT, TRNS,Y,   F,   G,   C,   R,   L,   SLSH,EQL, TRNS,TRNS,TRNS,TRNS,  \
-        TRNS,TRNS,O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     TRNS,                 \
-        TRNS,     SCLN,Q,   J,   K,   X,   B,   TRNS,W,   V,   Z,        TRNS,     TRNS,       \
+        TRNS,QUOT,COMM,DOT, P,   Y,   F,   G,   C,   R,   L,   SLSH,EQL, TRNS,TRNS,TRNS,TRNS,  \
+        TRNS,A,   O,   E,   U,   I,   D,   H,   T,   N,   S,   MINS,     TRNS,                 \
+        TRNS,     SCLN,Q,   J,   K,   X,   B,   M,   W,   V,   Z,        TRNS,     TRNS,       \
         TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS), \
-/* Keymap 3:  Media and Layer Select Keys
+/* Keymap 4:  Media and Layer Select Keys
  * ,---.   ,---------------. ,---------------. ,---------------. ,-----------.
- * |Fn4|   |   |   |   |   | |Cal|   |   |   | |   |Pwr|Slp|Wak| |Prv|Nxt|Mut|
+ * |Fn5|   |   |   |   |   | |Cal|   |   |   | |   |Pwr|Slp|Wak| |Prv|Nxt|Mut|
  * `---'   `---------------' `---------------' `---------------' `-----------'
  * ,-----------------------------------------------------------. ,-----------.
  * |   |   |   |   |   |   |   |   |   |   |   |   |   |       | |Rwd|Ffd|V+ |
  * |-----------------------------------------------------------| |-----------|
- * |     |Fn0|   |   |   |   |   |   |   |   |   |   |   |     | |Stp|Ply|V- |
+ * |     |Fn0|Fn2|   |   |   |   |   |   |   |   |   |   |     | |Stp|Ply|V- |
  * |-----------------------------------------------------------| `-----------'
- * |      |   |   |Fn1|   |   |   |   |   |   |   |   |        |
+ * |      |   |   |Fn3|   |   |   |   |   |   |   |   |        |
  * |-----------------------------------------------------------|     ,---.
- * |        |   |   |Fn2|   |   |   |   |   |   |   |          |     |   |
+ * |        |   |   |Fn1|   |   |   |   |   |   |   |          |     |   |
  * |-----------------------------------------------------------| ,-----------.
  * |   |   |   |                               |   |   |   |   | |   |   |   |
  * `-----------------------------------------------------------' `-----------'
  */
     KEYMAP( \
-        FN4,      TRNS,TRNS,TRNS,TRNS,CALC,TRNS,TRNS,TRNS,TRNS,PWR, SLEP,WAKE,MPRV,MNXT,MUTE,  \
+        FN5,      TRNS,TRNS,TRNS,TRNS,CALC,TRNS,TRNS,TRNS,TRNS,PWR, SLEP,WAKE,MPRV,MNXT,MUTE,  \
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MRWD,MFFD,VOLU,  \
-        TRNS,FN0, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MSTP,MPLY,VOLD,  \
-        TRNS,TRNS,TRNS,FN1, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,                 \
-        TRNS,     TRNS,FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,       \
+        TRNS,FN0, FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,MSTP,MPLY,VOLD,  \
+        TRNS,TRNS,TRNS,FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,                 \
+        TRNS,     TRNS,FN1, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,     TRNS,       \
         TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS), \
 };
 
 
-static const uint16_t PROGMEM fn_actions[] = {
+const uint16_t PROGMEM fn_actions[] = {
     [0] = ACTION_DEFAULT_LAYER_SET(0),
     [1] = ACTION_DEFAULT_LAYER_SET(1),
     [2] = ACTION_DEFAULT_LAYER_SET(2),
-    [3] = ACTION_LAYER_MOMENTARY(3),
-    [4] = ACTION_FUNCTION(TEENSY_KEY),
+    [3] = ACTION_DEFAULT_LAYER_SET(3),
+    [4] = ACTION_LAYER_MOMENTARY(4),
+    [5] = ACTION_FUNCTION(TEENSY_KEY),
 };
--- YAK87/keymap_tkl.h
+++ YAK87/keymap_tkl.h
@@ -16,6 +16,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 
+#ifndef KEYMAP_COMMON_H
+#define KEYMAP_COMMON_H
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <avr/pgmspace.h>
@@ -26,6 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "host.h"
 #include "debug.h"
 #include "keymap.h"
+
 
 /* Map physical keyboard layout to matrix array */
 #define KEYMAP( \
@@ -44,8 +48,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* 1 */   { KC_##K1A, KC_NO,    KC_##K1C, KC_##K1D, KC_##K1E, KC_##K1F, KC_##K1G, KC_##K1H, KC_##K1I, KC_##K1J, KC_##K1K, KC_##K1L, KC_NO,    KC_##K1N, KC_NO,    KC_##K1P, KC_NO   }, \
 /* 0 */   { KC_##K0A, KC_##K0B, KC_##K0C, KC_NO,    KC_NO,    KC_NO,    KC_##K0G, KC_NO,    KC_NO,    KC_NO,    KC_##K0K, KC_##K0L, KC_##K0M, KC_##K0N, KC_##K0O, KC_##K0P, KC_##K0Q}  \
 }
-
-#include "keymap.h"
 
 #define KEYMAPS_SIZE    (sizeof(keymaps) / sizeof(keymaps[0]))
 #define FN_ACTIONS_SIZE (sizeof(fn_actions) / sizeof(fn_actions[0]))
@@ -74,3 +76,5 @@ action_t keymap_fn_to_action(uint8_t keycode) {
 
     return action;
 }
+
+#endif  /* KEYMAP_COMMON_H */
